3af35742df657115e82e1cab6bd4845727caeb21
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-09-30 12:05:32 +0530

    Solved navigation error

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
       <img src={logo} alt='Logo' className='logo'></img>
       <div className='desktopMenu'>
         <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Home</Link>
-        <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>About</Link>
+        <Link activeClass='active' to='new-skills' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Skills</Link>
         <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Projects</Link>
       </div>
       <button className='desktopMenuBtn' onClick={() => {
@@ -25,7 +25,7 @@ const Navbar = () => {
       <img src={menu} alt='Menu' className='mobMenu' onClick={()=>setShowMenu(!showMenu)}></img>
       <div className='navMenu' style={{display: showMenu? 'flex':'none'}}>
         <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>Home</Link>
-        <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>About</Link>
+        <Link activeClass='active' to='new-skills' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>Skills</Link>
         <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>Projects</Link>
         <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>Contact</Link>
       </div>
